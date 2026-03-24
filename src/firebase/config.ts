87440236d2351222bd9ff93930008187204db39d
feng jiang <jiangfen106@gmail.com>
2026-03-24 03:15:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5138658952-d6f17",
+  "appId": "1:246847419931:web:e5798c2951fa00eff62abe",
+  "apiKey": "AIzaSyAHCRHm1fh1CBiaS3Zuj-bVtIZlG50aJm8",
+  "authDomain": "studio-5138658952-d6f17.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "246847419931"
+};
