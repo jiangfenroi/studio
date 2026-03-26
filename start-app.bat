36696f379e4548d26152bcf0e7e86c9997cc1944
feng jiang <jiangfen106@gmail.com>
2026-03-26 14:14:37 +0000

    readme更新   开机自动启动命令行即可，不用打开浏览器

--- start-app.bat
+++ start-app.bat
@@ -1,10 +1,8 @@
 @echo off
-title HealthInsight Server - 医疗内网后端控制台
+TITLE HealthInsight Registry - 医疗内网后端服务
 set NODE_SKIP_PLATFORM_CHECK=1
-echo [INFO] 正在启动 HealthInsight Registry 医疗中心服务...
-echo [INFO] 运行端口: 9002
-echo [INFO] 请勿关闭此窗口，关闭窗口将停止医疗服务。
+echo [INFO] 正在建立安全临床通信链路...
+echo [INFO] 正在针对 Windows 7 环境进行兼容性注入...
 echo.
-cd /d %~dp0
-npm run start
+npm start
 pause
