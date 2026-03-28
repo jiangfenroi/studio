1c3af7ade6881664b1c4bbc14518fb66406580e8
feng jiang <jiangfen106@gmail.com>
2026-03-28 12:42:13 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/components/forms/AbnormalResultForm.tsx
+++ src/components/forms/AbnormalResultForm.tsx
@@ -20,7 +20,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/form"
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
